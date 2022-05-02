1739cdab794840d9041464892a0e3b8bc24bf666
ASRAngel <ASRAngel2012@outlook.com>
2022-05-02 14:07:14 -0400

    Cleaning up

--- src/main/java/Driver/Main.java
+++ src/main/java/Driver/Main.java
@@ -28,59 +28,50 @@ public class Main {
 
     public static void main(String[] args) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
 
-//        String testLispCodeMethod = "(* 2 (cos 0) (+ 4 6))";
-//        String testLispCodeMethod2 = "5(+ 1 2)";
-//        String testLispCodeMethod3 = "(pprint (string-upcase foo))";
-//        String testLispCodeMethod4 = "(if x (format t \"yes\") (format t \"no\")))";
-//        String testLispCodeMethod5 = "(* (+ 1 2) (- 3 4))";
-//        String nestedLISP = "(defun print-list (list)" +
-//                "  (dolist (i list)" +
-//                "    (format t \"item: ~a~%\" i)))";
-//        String nestedLisp2 = """
-//                (defun read-lines (file)
-//                  "Return a list of lines in FILE."
-//                  (with-temp-buffer
-//                    (insert-file-contents file)
-//                    (split-string
-//                     (buffer-string) "" t)))
-//
-//                (mapcar
-//                 (lambda (x)
-//                   (mapcar
-//                    (lambda (y) (string-to-number y))
-//                    (split-string x " ")))
-//                 (read-lines "blob.txt"))\s""";
-//
-//        LispValidatorUsingDeque LISP = new LispValidatorUsingDeque();
-//        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod));
-//        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod2));
-//        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod3));
-//        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod4));
-//        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod5));
-//        System.out.println(LISP.isLISPCodeValid(nestedLISP));
-//        System.out.println(LISP.isLISPCodeValid(nestedLisp2));
-//        System.out.println(LISP.isLISPCodeValid(null)); //checking for null string
-//        System.out.println(LISP.isLISPCodeValid("")); //checking for empty string
+        String testLispCodeMethod = "(* 2 (cos 0) (+ 4 6))";
+        String testLispCodeMethod2 = "5(+ 1 2)";
+        String testLispCodeMethod3 = "(pprint (string-upcase foo))";
+        String testLispCodeMethod4 = "(if x (format t \"yes\") (format t \"no\")))";
+        String testLispCodeMethod5 = "(* (+ 1 2) (- 3 4))";
+        String nestedLISP = "(defun print-list (list)" +
+                "  (dolist (i list)" +
+                "    (format t \"item: ~a~%\" i)))";
+        String nestedLisp2 = """
+                (defun read-lines (file)
+                  "Return a list of lines in FILE."
+                  (with-temp-buffer
+                    (insert-file-contents file)
+                    (split-string
+                     (buffer-string) "" t)))
+
+                (mapcar
+                 (lambda (x)
+                   (mapcar
+                    (lambda (y) (string-to-number y))
+                    (split-string x " ")))
+                 (read-lines "blob.txt"))\s""";
+
+        LispValidatorUsingDeque LISP = new LispValidatorUsingDeque();
+        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod));
+        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod2));
+        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod3));
+        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod4));
+        System.out.println(LISP.isLISPCodeValid(testLispCodeMethod5));
+        System.out.println(LISP.isLISPCodeValid(nestedLISP));
+        System.out.println(LISP.isLISPCodeValid(nestedLisp2));
+        System.out.println(LISP.isLISPCodeValid(null)); //checking for null string
+        System.out.println(LISP.isLISPCodeValid("")); //checking for empty string
 
 
         EnrolleeCSVParser CSVParser = new EnrolleeCSVParser();
-        // EnrolleeCSVWriter writer = new EnrolleeCSVWriter();
         HelperMethod helper = new HelperMethod();
 
         List<User> userList = CSVParser.readCSVFile(); // reads whole file
-
         List<User> filteredUsers = helper.filterList(userList); //filters the null, empty fields
         userList.addAll(userHashMap.values());
-        System.out.println(userList.size());
-        System.out.println(userHashMap.values());
         companyList = helper.filterByInsuranceCompany(userList, companyList);
-        System.out.println(companyList);
-        System.out.println(userList.size());
 
-
-
-
-        for (User user : userList) {
+        for (User user : filteredUsers) {
             helper.mapCompanyToUser(user, userHashMap);
         }
 
@@ -89,7 +80,6 @@ public class Main {
             for (Map.Entry<String, User> entry : userHashMap.entrySet()) {
                 String s = entry.getKey();
                 User user = entry.getValue();
-
                 try {
                     if(user.getInsuranceCompany().equals(company)){
                         writer.write(String.valueOf(user));
